b1b53181dc74652a155cf6fa46a8fd92cf0d6ea5
konrad <ostrowskikonrad71@gmail.com>
2023-03-17 16:07:35 +0100

    new_static

--- my_resume/my_resume/settings.py
+++ my_resume/my_resume/settings.py
@@ -124,9 +124,18 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "main/static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "main/static",
+# ]
+
+# settings.py
+
+# ...
+
+STATIC_ROOT =  BASE_DIR / "main/static"
+
+# ...
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
